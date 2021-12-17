6326da07aa3377bf8b68f9d65f66d6219699071a
chalammm <tipanan_ji@cmu.ac.th>
2021-12-17 17:30:27 +0700

    completed lab11_3.cpp

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int A = 0;
+    double mean, dev, sum = 0, lol = 0;
+    ifstream source;
+    source.open("score.txt");
+    string score;
+    while(getline(source,score)){
+        sum += atof(score.c_str());
+        lol += pow(atof(score.c_str()),2);
+        A++; 
+    }
+    mean = sum/A;
+    dev = sqrt((lol/A) - pow(mean,2));
+    cout << "Number of data = "<< A << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = "<< dev;
+}
